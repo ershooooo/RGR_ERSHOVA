6e64b427a72a08ed895fe322255d2c3c992467a1
Clara <ershovaclara@yandex.ru>
2023-12-19 20:23:22 +0700

    Шаблоны

--- DB/models.py
+++ DB/models.py
@@ -17,20 +17,7 @@ class users(db.Model,UserMixin):
         return f'id:{self.id}, username:{self.username}'
         
 
-class articles(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
-    title = db.Column(db.String(50), nullable=False)
-    article_text = db.Column(db.Text, nullable=False)
-    is_favorite = db.Column(db.Boolean, default=False)
-    is_public = db.Column(db.Boolean, default=False)
-    likes = db.Column(db.Integer,default=0)
-    
-
-    def __repr__(self):
-        return f'id:{self.id},title:{self.title}, article_text:{self.article_text}'
-
-class Form(db.Model):
+class form(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(50), nullable=False)
     age = db.Column(db.Integer, nullable=False)
--- app.py
+++ app.py
@@ -1,30 +1,34 @@
-from flask import Flask, session
+from werkzeug.security import check_password_hash, generate_password_hash
+from flask import Flask, Blueprint, render_template, request, make_response, redirect
 from flask_sqlalchemy import SQLAlchemy
-from Db import db
-from Db.models import users
 from flask_login import LoginManager
-from main_rgz import main_rgz
+from Db import db
+from Db.models import users, form
+from flask_login import UserMixin
+from flask_login import login_user, login_required, current_user, logout_user
+
 
 app = Flask(__name__)
 
-app.secret_key='123'
+
+app.secret_key = '123'
 user_db = "ershova"
-host_ip = "127.0.0.1"
+host_ip = "localhost"
 host_port = "5432"
 database_name = "ershova_RGZ"
 password = "123"
 
+
 app.config['SQLALCHEMY_DATABASE_URI'] = f'postgresql://{user_db}:{password}@{host_ip}:{host_port}/{database_name}'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False 
 db.init_app(app)
+
 
 #Подключение Flask-Login
 login_manager=LoginManager()
 #Направление пользователя, если нет авторизации
-login_manager.login_view = 'lab6.login'
+login_manager.login_view = 'app.login'
 login_manager.init_app(app)
-#Где и как находить пользователя
 @login_manager.user_loader
 def load_users(user_id):
     return users.query.get(int(user_id))
-
